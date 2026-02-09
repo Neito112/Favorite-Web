4043012635b3f1db9746181a091955265352af6d
Neito <orenji11299@gmail.com>
2026-02-09 12:17:06 +0700

    Pre-release commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// Import file giao diện chính của bạn
+import AppEntry from './BookmarkManager.jsx' 
+import './index.css' // Import file CSS (nếu có Tailwind)
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppEntry />
+  </React.StrictMode>,
 )
